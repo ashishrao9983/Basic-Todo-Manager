93433632cd91eee2e4489caa3ec6816f738343c4
ashishrao9983 <ashishrao9983@gmail.com>
2025-06-17 17:19:23 +0530

    Fix: updated base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/smart-todo-manager/',
-  plugins: 
-  [ tailwindcss(),
-    react()],
+  base: '/smart-todo-manager/', // <-- this must match your repo name
+  plugins: [react(), tailwindcss()],
 })
