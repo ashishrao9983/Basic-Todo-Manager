7e121758a243ae4f6dd3cf094c0b8eb284ea0beb
ashishrao9983 <ashishrao9983@gmail.com>
2025-06-18 11:31:42 +0530

    your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/smart-todo-manager/', // <-- this must match your repo name
-  plugins: [react(), tailwindcss()],
+  base: '/smart-todo-manager/',
+  plugins: 
+  [ tailwindcss(),
+    react()],
 })
